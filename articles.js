196e1bda8be407c4bc0261cca50c1905affcbdb5
Patrick God <patrick@godknows.de>
2019-07-16 18:40:16 +0200

    GET single article.

--- articles.js
+++ articles.js
@@ -2,6 +2,12 @@ const ARTICLES = require("./mock-articles");
 
 module.exports = function(app) {
   app.get("/articles", function(request, response) {
-      response.send(ARTICLES);
+    response.send(ARTICLES);
+  });
+
+  app.get("/articles/:key", function(request, response) {
+    response.send(
+      ARTICLES.filter(article => article.key === request.params.key)[0]
+    );
   });
 };
