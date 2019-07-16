e87d1c6aab95c476c584791182ffbf77e5232a63
Patrick God <patrick@godknows.de>
2019-07-16 19:12:30 +0200

    360 implementing cors

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
+
+var corsOptions = {
+    origin: ["http://localhost:4200", "http://localhost:4000"]
+}
+
+app.use(cors(corsOptions));
 
 app.listen(8000, () => {
     console.log("Server is started and listening.");
