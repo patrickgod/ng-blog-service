2d4d10daaa2f9f7430db732058b3f695d027133f
Patrick God <patrick@godknows.de>
2019-07-25 07:03:07 +0200

    modelling the articles table

--- sql.js
+++ sql.js
@@ -5,8 +5,17 @@ const sequelize = new Sequelize("ngblog", "root", "123456", {
   dialect: "mariadb",
   port: 3308,
   dialectOptions: {
-      timezone: process.env.db_timezone
+    timezone: process.env.db_timezone
   }
+});
+
+const Article = sequelize.define("article", {
+  title: { type: Sequelize.STRING },
+  key: { type: Sequelize.STRING },
+  date: { type: Sequelize.DATE },
+  content: { type: Sequelize.TEXT },
+  description: { type: Sequelize.TEXT },
+  imageUrl: { type: Sequelize.STRING }
 });
 
 init = function() {
@@ -18,6 +27,8 @@ init = function() {
     .catch(err => {
       console.error("Unable to connect to the database: ", err);
     });
+
+    Article.sync();
 };
 
 module.exports.init = init;
