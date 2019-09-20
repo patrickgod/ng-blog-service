74947917211dec1c785c4808f9a6c2c84337c733
Patrick God <patrick@godknows.de>
2019-09-20 21:13:26 +0200

    Angular Route Guard

--- auth.js
+++ auth.js
@@ -29,4 +29,9 @@ module.exports = function(app, sql) {
       }
     });
   });
+
+  app.post("/user/auth", function(request, response) {
+    var valid = jwtUtil.verifyJwt(request.body.token);
+    response.send(valid != false);
+  })
 };
